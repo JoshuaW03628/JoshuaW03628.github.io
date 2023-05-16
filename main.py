144a2a02e304aa018e3e72bd2a4838a67ca9cf6b
Joshua Williams <joshuawilliams52006@gmail.com>
2023-05-16 13:05:30 -0700

    Initial commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from __init__ import app  # Definitions initialization
 from api.joke import joke_api # Blueprint import api definition
 from api.covid import covid_api # Blueprint import api definition
 
-from bp_projects.projects import app_projects # Blueprint directory import projects definition
+from bp_projects import app_projects # Blueprint directory import projects definition
 
 app.register_blueprint(joke_api) # register api routes
 app.register_blueprint(covid_api) # register api routes
